dee1678c0d5c53012ad8069815fc751ff8843ab8
unknown <andressapatricio93@gmail.com>
2024-02-17 16:44:00 -0300

    NavBar, Footer, Login

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+
+import Footer from './components/footer/Footer';
+import Navbar from './components/navBar/NavBar';
+import Home from './paginas/home/Home';
+
+function App() {
+  return (
+    <>
+    <Navbar/>
+    <Home/>
+    <Footer/>
+    </>
+);
+}
+export default App;
